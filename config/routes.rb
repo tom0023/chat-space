9999367e3e7aa50302a8a14d3c4b877d8f74d765
tom0023 <tomita0720shinichi@gmail.com>
2020-06-26 15:31:16 +0900

    group.commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update, :destroy]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
